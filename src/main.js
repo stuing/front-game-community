8fe2703a6f2312881448a36cb3bf2fc47da06153
sidi <sidi@123.cn>
2023-08-07 15:29:21 +0800

    初始化Vue3项目,加入Vant组件库

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// import Button from 'vant/lib/button';
+// import 'vant/lib/button/style';
 
-createApp(App).mount('#app')
+createApp(App).use(Vant).mount('#app')
+// createApp(App).use(Button).mount('#app')
